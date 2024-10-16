a3784e5704d958813f6c20fa3dc943cd8ae4cf40
TosTar26 <ozsanchez2004@gmail.com>
2024-10-15 21:47:11 -0600

    fix: Fixing some bugs

--- nuestra_señora_del_rosario/src/components/specific/AddGuardianForm.tsx
+++ nuestra_señora_del_rosario/src/components/specific/AddGuardianForm.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useGuardianMutation } from '../../hooks/useGuardian'; // Hook para la mutación de guardián
 import { useToast } from '../../hooks/useToast'; // Hook para mostrar notificaciones (Toast)
 import { Guardian } from '../../types/GuardianType'; // Importamos el tipo Guardian
+import { useThemeDark } from '../../hooks/useThemeDark'; // Hook para detectar modo oscuro
+import Toast from '../common/Toast';
 
 interface AddGuardianFormProps {
   setIsGuardianAdded: (added: boolean) => void;
@@ -10,7 +12,8 @@ interface AddGuardianFormProps {
 
 function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormProps) {
   const { mutate: saveGuardian, isLoading } = useGuardianMutation();
-  const { showToast } = useToast();
+  const { showToast, message, type } = useToast();
+  const { isDarkMode } = useThemeDark(); // Usar hook para detectar modo oscuro
 
   const [guardianData, setGuardianData] = useState<Partial<Guardian>>({
     name_GD: '',
@@ -26,12 +29,9 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
 
     saveGuardian(guardianData as Guardian, {
       onSuccess: (response) => {
-        console.log('Response from server:', response); // Log para ver la respuesta del servidor
         const id = response.data?.id_Guardian;
-        
         if (id) {
-          console.log('Guardian ID received:', id); // Verificar si recibimos el ID
-          setGuardianId(id); // Log para ver si se llama a setGuardianId
+          setGuardianId(id);
           setIsGuardianAdded(true);
           showToast('Guardián añadido exitosamente', 'success');
         } else {
@@ -44,19 +44,20 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
       },
     });
   };
+
   return (
-    <div className="w-full max-w-[1169px] mx-auto p-6 bg-white rounded-[20px] shadow-2xl">
-      <h2 className="text-3xl font-bold text-center mb-8">Añadir Guardián</h2>
+    <div className={`w-full max-w-[1169px] mx-auto p-6 rounded-[20px] shadow-2xl ${isDarkMode ? 'bg-[#0D313F] text-white' : 'bg-white text-gray-800'}`}>
+      <h2 className={`text-3xl font-bold text-center mb-8 ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>Añadir Encargado</h2>
 
       <form onSubmit={handleFormSubmit} className="grid grid-cols-2 gap-6">
         {/* Nombre del guardián */}
         <div>
-          <label className="block mb-2 text-lg">Nombre del Guardián</label>
+          <label className="block mb-2 text-lg">Nombre del Encargado</label>
           <input
             type="text"
             value={guardianData.name_GD}
             onChange={(e) => setGuardianData({ ...guardianData, name_GD: e.target.value })}
-            className="w-full p-3 rounded-md bg-gray-200"
+            className={`w-full p-3 rounded-md ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200'}`}
             required
           />
         </div>
@@ -68,7 +69,7 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
             type="text"
             value={guardianData.lastname1_GD}
             onChange={(e) => setGuardianData({ ...guardianData, lastname1_GD: e.target.value })}
-            className="w-full p-3 rounded-md bg-gray-200"
+            className={`w-full p-3 rounded-md ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200'}`}
             required
           />
         </div>
@@ -80,7 +81,7 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
             type="text"
             value={guardianData.lastname2_GD}
             onChange={(e) => setGuardianData({ ...guardianData, lastname2_GD: e.target.value })}
-            className="w-full p-3 rounded-md bg-gray-200"
+            className={`w-full p-3 rounded-md ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200'}`}
           />
         </div>
 
@@ -91,7 +92,7 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
             type="text"
             value={guardianData.cedula_GD}
             onChange={(e) => setGuardianData({ ...guardianData, cedula_GD: e.target.value })}
-            className="w-full p-3 rounded-md bg-gray-200"
+            className={`w-full p-3 rounded-md ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200'}`}
             required
           />
         </div>
@@ -103,7 +104,7 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
             type="email"
             value={guardianData.email_GD}
             onChange={(e) => setGuardianData({ ...guardianData, email_GD: e.target.value })}
-            className="w-full p-3 rounded-md bg-gray-200"
+            className={`w-full p-3 rounded-md ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200'}`}
           />
         </div>
 
@@ -114,7 +115,7 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
             type="text"
             value={guardianData.phone_GD}
             onChange={(e) => setGuardianData({ ...guardianData, phone_GD: e.target.value })}
-            className="w-full p-3 rounded-md bg-gray-200"
+            className={`w-full p-3 rounded-md ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200'}`}
             required
           />
         </div>
@@ -125,12 +126,13 @@ function AddGuardianForm({ setIsGuardianAdded, setGuardianId }: AddGuardianFormP
           <button
             type="submit"
             disabled={isLoading}
-            className="px-7 py-4 bg-blue-500 text-white text-lg rounded-lg shadow-lg hover:bg-blue-600 transition duration-200"
+            className={`px-7 py-4 rounded-lg shadow-lg transition duration-200 ${isLoading ? 'bg-gray-400' : isDarkMode ? 'bg-blue-500 hover:bg-blue-600' : 'bg-blue-600 hover:bg-blue-700'} text-white`}
           >
-            {isLoading ? 'Guardando...' : 'Guardar Guardián'}
+            {isLoading ? 'Guardando...' : 'Guardar Encargado'}
           </button>
         </div>
       </form>
+    <Toast message={message} type={type} />
     </div>
   );
 }
--- nuestra_señora_del_rosario/src/components/specific/ResidentList.tsx
+++ nuestra_señora_del_rosario/src/components/specific/ResidentList.tsx
@@ -6,6 +6,7 @@ import { Resident } from '../../types/ResidentsType';
 import { useRoom } from '../../hooks/useRoom';
 import { useDependencyLevel } from '../../hooks/useDependencyLevel';
 import { useUpdateResidentDetails } from '../../hooks/useUpdateResidentDetails';
+import { useThemeDark } from '../../hooks/useThemeDark'; // Hook para modo oscuro
 
 // Helper para formatear las fechas (YYYY-MM-DD)
 const formatDate = (dateString: string) => {
@@ -13,6 +14,9 @@ const formatDate = (dateString: string) => {
 };
 
 function ResidentList() {
+  // Obtenemos el estado de modo oscuro
+  const { isDarkMode } = useThemeDark();
+
   // Obtenemos la función refetch
   const { data: residents = [], isLoading, error, refetch } = useResidents();
   const { data: rooms = [] } = useRoom();
@@ -108,19 +112,19 @@ function ResidentList() {
   }
 
   return (
-    <div className="w-full max-w-[1169px] mx-auto p-6 bg-white rounded-[20px] shadow-2xl">
+    <div className={`w-full max-w-[1169px] mx-auto p-6 rounded-[20px] shadow-2xl ${isDarkMode ? 'bg-[#0D313F] text-white' : 'bg-white text-gray-800'}`}>
       <div className="flex justify-between items-center mb-8">
-        <h2 className="text-3xl font-bold text-center">Listado de Residentes</h2>
+        <h2 className={`text-3xl font-bold text-center ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>Listado de Residentes</h2>
         <div className="flex space-x-4">
           <button
             onClick={() => navigate('/dashboard/solicitudesAprobadas')}
-            className="px-6 py-2 bg-blue-800 text-white rounded-lg hover:bg-blue-900 transition duration-200"
+            className={`px-6 py-2 rounded-lg transition duration-200 ${isDarkMode ? 'bg-blue-600 hover:bg-blue-700' : 'bg-blue-800 hover:bg-blue-900'} text-white`}
           >
             Solicitudes Aprobadas
           </button>
           <button
             onClick={() => navigate('/dashboard/NuevoResidente')}
-            className="px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition duration-200"
+            className={`px-6 py-2 rounded-lg transition duration-200 ${isDarkMode ? 'bg-green-500 hover:bg-green-600' : 'bg-green-600 hover:bg-green-700'} text-white`}
           >
             Nuevo Residente
           </button>
@@ -134,16 +138,16 @@ function ResidentList() {
           placeholder="Buscar por nombre, apellido o cédula"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full max-w-md p-3 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+          className={`w-full max-w-md p-3 border rounded-lg focus:outline-none focus:ring-2 ${isDarkMode ? 'bg-gray-700 text-white focus:ring-blue-400' : 'text-gray-700 focus:ring-blue-600'}`}
         />
       </div>
 
       {/* Tabla de Residentes */}
       {filteredResidents.length > 0 ? (
         <div className="flex justify-center">
-          <table className="min-w-full max-w-5xl bg-transparent rounded-lg shadow-md">
+          <table className={`min-w-full max-w-5xl rounded-lg shadow-md ${isDarkMode ? 'bg-[#0D313F]' : 'bg-white'}`}>
             <thead>
-              <tr className="bg-gray-100 text-center">
+              <tr className={`text-center ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-100 text-gray-800'}`}>
                 <th className="p-4">Nombre</th>
                 <th className="p-4">Primer Apellido</th>
                 <th className="p-4">Segundo Apellido</th>
@@ -153,7 +157,7 @@ function ResidentList() {
             </thead>
             <tbody>
               {filteredResidents.map((resident: Resident) => (
-                <tr key={resident.id_Resident} className="bg-white text-gray-800 hover:bg-gray-200 text-center">
+                <tr key={resident.id_Resident} className={`text-center ${isDarkMode ? 'bg-gray-700 text-white hover:bg-gray-600' : 'bg-white text-gray-800 hover:bg-gray-200'}`}>
                   <td className="p-4">{resident.name_AP}</td>
                   <td className="p-4">{resident.lastname1_AP}</td>
                   <td className="p-4">{resident.lastname2_AP}</td>
@@ -161,7 +165,7 @@ function ResidentList() {
                   <td className="p-4">
                     <button
                       onClick={() => handleShowDetails(resident)}
-                      className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+                      className={`px-4 py-2 rounded-lg transition duration-200 ${isDarkMode ? 'bg-blue-500 hover:bg-blue-600' : 'bg-blue-600 hover:bg-blue-700'} text-white`}
                     >
                       Ver Más Detalles
                     </button>
@@ -175,153 +179,153 @@ function ResidentList() {
         <div className="text-center text-gray-500">No se encontraron residentes.</div>
       )}
 
-      {showModal && selectedResident && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-4xl">
-            <h3 className="text-2xl font-bold mb-6">Detalles del Residente</h3>
+{showModal && selectedResident && (
+  <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+    <div className={`rounded-lg shadow-lg p-8 w-full max-w-4xl ${isDarkMode ? 'bg-[#0D313F] text-white' : 'bg-white text-gray-800'}`}>
+      <h3 className="text-2xl font-bold mb-6">Detalles del Residente</h3>
 
-            <div className="grid grid-cols-2 gap-4">
-              {/* Número de habitación */}
-              <div>
-                <label className="block font-bold">Habitación:</label>
-                <select
-                  value={idRoom}
-                  disabled={!isEditing}
-                  onChange={(e) => setIdRoom(Number(e.target.value))}
-                  className={`w-full p-2 mt-1 border rounded-md ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
-                >
-                  {rooms.map((room) => (
-                    <option key={room.id_Room} value={room.id_Room}>
-                      {room.roomNumber}
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              {/* Grado de Dependencia */}
-              <div>
-                <label className="block font-bold">Grado de Dependencia:</label>
-                <select
-                  value={idDependencyLevel}
-                  disabled={!isEditing}
-                  onChange={(e) => setIdDependencyLevel(Number(e.target.value))}
-                  className={`w-full p-2 mt-1 border rounded-md ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
-                >
-                  {dependencyLevels.map((level) => (
-                    <option key={level.id_DependencyLevel} value={level.id_DependencyLevel}>
-                      {level.levelName}
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              {/* Sexo */}
-              <div>
-                <label className="block font-bold">Sexo:</label>
-                <input
-                  type="text"
-                  value={sexo}
-                  readOnly={!isEditing}
-                  onChange={(e) => setSexo(e.target.value)}
-                  className={`w-full p-2 mt-1 border rounded-md ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
-                />
-              </div>
-
-              {/* Fecha de Nacimiento */}
-              <div>
-                <label className="block font-bold">Fecha de Nacimiento:</label>
-                <input
-                  type="text"
-                  value={formatDate(selectedResident.fechaNacimiento)} 
-                  className="w-full p-2 mt-1 border rounded-md bg-gray-200 cursor-not-allowed"
-                  readOnly
-                />
-              </div>
-
-              {/* Estado */}
-              <div>
-                <label className="block font-bold">Estado:</label>
-                <input
-                  type="text"
-                  value={status}
-                  readOnly={!isEditing}
-                  onChange={(e) => setStatus(e.target.value)}
-                  className={`w-full p-2 mt-1 border rounded-md ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
-                />
-              </div>
-
-              {/* Fecha de Entrada */}
-              <div>
-                <label className="block font-bold">Fecha de Entrada:</label>
-                <input
-                  type="text"
-                  value={formatDate(selectedResident.entryDate)}
-                  className="w-full p-2 mt-1 border rounded-md bg-gray-200 cursor-not-allowed"
-                  readOnly
-                />
-              </div>
-
-              {/* Edad */}
-              <div>
-                <label className="block font-bold">Edad:</label>
-                <input
-                  type="text"
-                  value={selectedResident.edad.toString()}
-                  className="w-full p-2 mt-1 border rounded-md bg-gray-200 cursor-not-allowed"
-                  readOnly
-                />
-              </div>
-
-              {/* Nombre del Guardián */}
-              <div>
-                <label className="block font-bold">Nombre del Guardián:</label>
-                <input
-                  type="text"
-                  value={selectedResident.guardianName}
-                  className="w-full p-2 mt-1 border rounded-md bg-gray-200 cursor-not-allowed"
-                  readOnly
-                />
-              </div>
-
-              {/* Teléfono del Guardián */}
-              <div>
-                <label className="block font-bold">Teléfono del Guardián:</label>
-                <input
-                  type="text"
-                  value={selectedResident.guardianPhone}
-                  className="w-full p-2 mt-1 border rounded-md bg-gray-200 cursor-not-allowed"
-                  readOnly
-                />
-              </div>
-            </div>
-
-            <div className="mt-6 flex justify-end">
-              <button
-                onClick={handleCloseDetails}
-                className="px-6 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition duration-200"
-              >
-                {isEditing ? 'Cancelar' : 'Cerrar'}
-              </button>
-              {!isEditing ? (
-                <button
-                  onClick={handleEditClick}
-                  className="ml-4 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
-                >
-                  Editar
-                </button>
-              ) : (
-                <button
-                  onClick={handleUpdateClick}
-                  disabled={isUpdating}
-                  className="ml-4 px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition duration-200"
-                >
-                  {isUpdating ? 'Guardando...' : 'Guardar'}
-                </button>
-              )}
-            </div>
-          </div>
+      <div className="grid grid-cols-2 gap-4">
+        {/* Número de habitación */}
+        <div>
+          <label className="block font-bold">Habitación:</label>
+          <select
+            value={idRoom}
+            disabled={!isEditing}
+            onChange={(e) => setIdRoom(Number(e.target.value))}
+            className={`w-full p-2 mt-1 border rounded-md ${isEditing ? (isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200') : 'bg-gray-500 text-white cursor-not-allowed'}`}
+          >
+            {rooms.map((room) => (
+              <option key={room.id_Room} value={room.id_Room}>
+                {room.roomNumber}
+              </option>
+            ))}
+          </select>
         </div>
-      )}
+
+        {/* Grado de Dependencia */}
+        <div>
+          <label className="block font-bold">Grado de Dependencia:</label>
+          <select
+            value={idDependencyLevel}
+            disabled={!isEditing}
+            onChange={(e) => setIdDependencyLevel(Number(e.target.value))}
+            className={`w-full p-2 mt-1 border rounded-md ${isEditing ? (isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200') : 'bg-gray-500 text-white cursor-not-allowed'}`}
+          >
+            {dependencyLevels.map((level) => (
+              <option key={level.id_DependencyLevel} value={level.id_DependencyLevel}>
+                {level.levelName}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Sexo */}
+        <div>
+          <label className="block font-bold">Sexo:</label>
+          <input
+            type="text"
+            value={sexo}
+            readOnly={!isEditing}
+            onChange={(e) => setSexo(e.target.value)}
+            className={`w-full p-2 mt-1 border rounded-md ${isEditing ? (isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200') : 'bg-gray-500 text-white cursor-not-allowed'}`}
+          />
+        </div>
+
+        {/* Fecha de Nacimiento */}
+        <div>
+          <label className="block font-bold">Fecha de Nacimiento:</label>
+          <input
+            type="text"
+            value={formatDate(selectedResident.fechaNacimiento)} 
+            className={`w-full p-2 mt-1 border rounded-md bg-gray-500 text-white cursor-not-allowed`}
+            readOnly
+          />
+        </div>
+
+        {/* Estado */}
+        <div>
+          <label className="block font-bold">Estado:</label>
+          <input
+            type="text"
+            value={status}
+            readOnly={!isEditing}
+            onChange={(e) => setStatus(e.target.value)}
+            className={`w-full p-2 mt-1 border rounded-md ${isEditing ? (isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-200') : 'bg-gray-500 text-white cursor-not-allowed'}`}
+          />
+        </div>
+
+        {/* Fecha de Entrada */}
+        <div>
+          <label className="block font-bold">Fecha de Entrada:</label>
+          <input
+            type="text"
+            value={formatDate(selectedResident.entryDate)}
+            className={`w-full p-2 mt-1 border rounded-md bg-gray-500 text-white cursor-not-allowed`}
+            readOnly
+          />
+        </div>
+
+        {/* Edad */}
+        <div>
+          <label className="block font-bold">Edad:</label>
+          <input
+            type="text"
+            value={selectedResident.edad.toString()}
+            className={`w-full p-2 mt-1 border rounded-md bg-gray-500 text-white cursor-not-allowed`}
+            readOnly
+          />
+        </div>
+
+        {/* Nombre del Guardián */}
+        <div>
+          <label className="block font-bold">Nombre del Guardián:</label>
+          <input
+            type="text"
+            value={selectedResident.guardianName}
+            className={`w-full p-2 mt-1 border rounded-md bg-gray-500 text-white cursor-not-allowed`}
+            readOnly
+          />
+        </div>
+
+        {/* Teléfono del Guardián */}
+        <div>
+          <label className="block font-bold">Teléfono del Guardián:</label>
+          <input
+            type="text"
+            value={selectedResident.guardianPhone}
+            className={`w-full p-2 mt-1 border rounded-md bg-gray-500 text-white cursor-not-allowed`}
+            readOnly
+          />
+        </div>
+      </div>
+
+      <div className="mt-6 flex justify-end">
+        <button
+          onClick={handleCloseDetails}
+          className={`px-6 py-2 rounded-lg transition duration-200 ${isDarkMode ? 'bg-gray-500 hover:bg-gray-600' : 'bg-gray-600 hover:bg-gray-700'} text-white`}
+        >
+          {isEditing ? 'Cancelar' : 'Cerrar'}
+        </button>
+        {!isEditing ? (
+          <button
+            onClick={handleEditClick}
+            className={`ml-4 px-6 py-2 rounded-lg transition duration-200 ${isDarkMode ? 'bg-blue-500 hover:bg-blue-600' : 'bg-blue-600 hover:bg-blue-700'} text-white`}
+          >
+            Editar
+          </button>
+        ) : (
+          <button
+            onClick={handleUpdateClick}
+            disabled={isUpdating}
+            className={`ml-4 px-6 py-2 rounded-lg transition duration-200 ${isDarkMode ? 'bg-green-500 hover:bg-green-600' : 'bg-green-600 hover:bg-green-700'} text-white`}
+          >
+            {isUpdating ? 'Guardando...' : 'Guardar'}
+          </button>
+        )}
+      </div>
+    </div>
+  </div>
+)}
     </div>
   );
 }
