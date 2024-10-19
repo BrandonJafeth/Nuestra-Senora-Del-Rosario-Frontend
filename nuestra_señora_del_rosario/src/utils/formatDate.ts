a9aba63fa8fbe5a1e82b74fd1c7933bb2f19914a
TosTar26 <ozsanchez2004@gmail.com>
2024-10-18 21:46:02 -0600

    fix: fixing format dat and time

--- nuestra_señora_del_rosario/src/utils/formatDate.ts
+++ nuestra_señora_del_rosario/src/utils/formatDate.ts
@@ -1,6 +1,21 @@
+// src/utils/dateFormatter.ts
+import { format, parseISO } from 'date-fns';
+import { es } from 'date-fns/locale';
 
-export const formatDate = (date: string): string => {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(date).toLocaleDateString(undefined, options);
-  };
-  
+/**
+ * Formatea una fecha ISO a 'dd/MM/yyyy'
+ */
+export const formatDate = (isoDate: string): string => {
+  return format(parseISO(isoDate), 'dd/MM/yyyy', { locale: es });
+};
+
+/**
+ * Formatea la hora del formato 'HH:mm:ss' a 'HH:mm'
+ */
+export const formatTime = (time: string): string => {
+  return time.substring(0, 5); // Ejemplo: "14:30"
+};
+
+export const formatLongDate = (isoDate: string): string => {
+  return format(parseISO(isoDate), 'PPP', { locale: es });
+};
