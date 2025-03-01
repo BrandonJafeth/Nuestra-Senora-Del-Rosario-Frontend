0ecfb6b8a18c46fa858c4b0f59a96811b51f0102
TosTar21 <oscar.zuniga.sanchez@est.una.ac.cr>
2025-03-01 10:33:06 -0600

    feat: I made the edit the patologies

--- nuestra_señora_del_rosario/src/hooks/usePathology.ts
+++ nuestra_señora_del_rosario/src/hooks/usePathology.ts
@@ -1,9 +1,28 @@
 import { useQuery } from "react-query";
-import pathologysService from "../services/PathologyService";
+import { Pathology } from "../types/PathologyType";
+import ApiService from "../services/GenericService/ApiService";
+
+const apiService = new ApiService<Pathology>();
 
 export const usePathologies = () => {
-    return useQuery('pathologies', pathologysService.getAllPathologies, {
-      staleTime: 1000 * 60 * 5, // Cache de 5 minutos
-      refetchOnWindowFocus: false,
-    });
-  };
+  return useQuery<Pathology[], Error>(
+    "Pathology", // ✅ Debe coincidir con la key en `invalidateQueries`
+    async () => {
+      const response = await apiService.getAll("Pathology");
+
+      if (!response.data || !Array.isArray(response.data)) {
+        console.error("🚨 Error: Datos de patologías no válidos", response);
+        return [];
+      }
+
+      return response.data.map((item) => ({
+        id_Pathology: item.id_Pathology ?? 0,
+        name_Pathology: item.name_Pathology || "Desconocido",
+      }));
+    },
+    {
+      staleTime: 5 * 60 * 1000, // Cache por 5 minutos
+      cacheTime: 10 * 60 * 1000, // Almacenar en caché por 10 minutos
+    }
+  );
+};
