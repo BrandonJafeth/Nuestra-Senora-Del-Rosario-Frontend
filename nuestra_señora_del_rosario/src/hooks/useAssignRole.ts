64318a6b7b26c31c4c0089a87e7cc132190a48e8
TosTar21 <oscar.zuniga.sanchez@est.una.ac.cr>
2025-03-25 14:45:33 -0600

    fixed: I made that user modifications refresh in real time

--- nuestra_señora_del_rosario/src/hooks/useAssignRole.ts
+++ nuestra_señora_del_rosario/src/hooks/useAssignRole.ts
@@ -1,15 +1,23 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import assignRoleService from '../services/AssignRoleService';
 import { AssignRoleRequest, AssignRoleResponse } from '../types/UserType';
 
 export const useAssignRole = () => {
+  const queryClient = useQueryClient();
+
   return useMutation<AssignRoleResponse, Error, AssignRoleRequest>(
     async (roleData: AssignRoleRequest): Promise<AssignRoleResponse> => {
       const response = await assignRoleService.assignRoleToUser(roleData);
       return {
-        success: true, // Asumimos éxito si la petición no lanza error
+        success: true,
         message: (response.data as AssignRoleResponse).message
       };
+    },
+    {
+      onSuccess: () => {
+        // Invalida la query de usuarios para que se recargue
+        queryClient.invalidateQueries(['PaginatedUsers']);
+      }
     }
   );
 };
--- nuestra_señora_del_rosario/src/hooks/useConfigUser.ts
+++ nuestra_señora_del_rosario/src/hooks/useConfigUser.ts
@@ -1,13 +1,20 @@
-
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import userConfigService from "../services/UserConfigService";
 import { UpdateUserStatus } from "../types/UserConfigType";
 import { AxiosError, AxiosResponse } from "axios";
 
 export const useUpdateUserStatus = () => {
+  const queryClient = useQueryClient();
+
   return useMutation<AxiosResponse<void>, AxiosError, { userId: number; status: UpdateUserStatus }>(
     async ({ userId, status }: { userId: number; status: UpdateUserStatus }) => {
       return await userConfigService.updateUserStatus(userId, status);
+    },
+    {
+      onSuccess: () => {
+        // Invalida la query que trae la lista de usuarios para que se refresque
+        queryClient.invalidateQueries(['PaginatedUsers']);
+      }
     }
   );
 };
